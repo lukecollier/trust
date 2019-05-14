3a9a8c1d3300f22564ab74789a414387252e7b8b
Luke Collier <leccollier@gmail.com>
2019-05-14 17:57:19 +0100

    can now handle more then one root from the xml

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use quick_xml::Reader;
-use std::path::Path;
 use quick_xml::events::Event;
+
+use std::path::Path;
 use std::str;
+use std::fmt;
 
 
 struct Session<'a> {
@@ -31,6 +33,14 @@ impl Pane {
     }
 }
 
+impl fmt::Display for Pane {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // write!(f, "({}, {})", self.x, self.y);
+        // the output should be as a tree that scales depending on size of console
+        unimplemented!()
+    }
+}
+
 #[derive(Debug)]
 enum TmuxType {
     Session,
@@ -50,26 +60,36 @@ fn get_tmux_type(depth: &u8) -> TmuxType {
 struct State {
     depth: u8,
     prev_depth: u8,
-    children: Vec<Pane>
+    children: Vec<Pane>,
+    root: Vec<Pane>,
 }
 
 impl State {
     fn new() -> State {
-        State { depth: 0, prev_depth: 0, children: Vec::new() }
+        State { 
+            depth: 0, 
+            prev_depth: 0, 
+            children: Vec::new(), 
+            root: Vec::new() 
+        }
     }
+}
+
+fn is_root(depth: u8) -> bool {
+    depth == 0
 }
 
 fn handle_event<'a>(event: Event<'a>, state: &mut State) -> () {
     match event {
-        Event::Start(ref _e) => {
+        Event::Start(ref e) => {
+            println!("{} {} {}", str::from_utf8(e.name()).unwrap(), state.depth, state.prev_depth);
             state.prev_depth = state.depth.clone();
             state.depth += 1;
         },
-        Event::Text(ref _e) => {
-            ()
-        },
+        Event::Text(ref _e) => { () },
         Event::End(ref e) => {
             state.depth -= 1;
+            println!("/{} {} {}", str::from_utf8(e.name()).unwrap(), state.depth, state.prev_depth);
             let name = String::from(str::from_utf8(e.name()).unwrap());
             if state.prev_depth <= state.depth {
                 state.children.push(Pane::new(name));
@@ -77,10 +97,33 @@ fn handle_event<'a>(event: Event<'a>, state: &mut State) -> () {
                 let mut parent = Pane::new(name);
                 let children_add = state.children.split_off(0);
                 parent.push_all(children_add);
-                state.children.push(parent);
+                if is_root(state.depth) {
+                    state.root.push(parent);
+                } else {
+                    state.children.push(parent);
+                }
             }
         },
         _ => {()}
+    }
+}
+
+fn parse_into_roots<B: std::io::BufRead>(reader: &mut Reader<B>) -> () {
+    let mut buf = Vec::new();
+    let mut state = State::new();
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Eof) => break, // break loop when event at end of file
+            Ok(event) => handle_event(event, &mut state), 
+            Err(error) => panic!("Error at position {}: {:?}", reader.buffer_position(), error),
+        }
+        buf.clear(); // keep memory usage low
+    }
+
+    let mut i = 0;
+    for x in &state.root {
+        i += 1;
+        println!("{}: {:?}", i, x);
     }
 }
 
@@ -88,21 +131,5 @@ fn main() {
     let path = Path::new("./src/resources/test.xml");
     let mut reader = Reader::from_file(path).expect("failed to read");
     reader.trim_text(true);
-
-    let mut buf = Vec::new();
-
-    let mut state = State::new();
-
-    loop {
-        match reader.read_event(&mut buf) {
-            Ok(Event::Eof) => break,
-            Ok(event) => handle_event(event, &mut state), 
-            Err(error) => panic!("Error at position {}: {:?}", reader.buffer_position(), error),
-        }
-
-        // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
-        buf.clear();
-    }
-    println!("work {:?}", state.children);
-
+    parse_into_roots(&mut reader);
 }
