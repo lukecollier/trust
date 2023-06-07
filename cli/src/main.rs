ecb6f7c239ff5a359cb38b06d2bd6ea60b5caa40
lukecollier <lcollier@skedulo.com>
2023-06-08 00:11:02 +0100

    feat: Trust me, it's a good idea

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,37 +1,23 @@
-use trust_core::runner::TmuxIO;
-use trust_core::tmux::{Target, has_session};
-use trust_core::parser::{Parser};
-use dirs::{home_dir};
-
-use structopt::StructOpt;
+use dirs::home_dir;
 use std::path::PathBuf;
 
+use clap::Parser;
+
 /// Your trusty session manager
-#[derive(StructOpt, Debug)]
-#[structopt(name = "trust")]
-struct Opt {
-    // A flag, true if used in the command line. Note doc comment will
-    // be used for the help message of the flag.
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
     /// Activate debug mode
-    #[structopt(short = "d", long = "debug")]
+    #[arg(short, long = "debug")]
     debug: bool,
 
-    // The number of occurrences of the `v/verbose` flag
-    /// Verbose mode (-v, -vv, -vvv, etc.)
-    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
-    verbose: u8,
-
     /// Path to config file
-    #[structopt(short = "c", long = "config", parse(from_os_str))]
+    #[arg(short, long = "config")]
     config: Option<PathBuf>,
-
-    /// Session name to attach too
-    #[structopt(name = "SESSION")]
-    session_name: String,
 }
 
 fn main() {
-    let opt = Opt::from_args();
+    let opt = Cli::parse();
     if opt.config.is_none() {
         match home_dir() {
             Some(mut path) => {
@@ -39,29 +25,18 @@ fn main() {
                 path.push("trust");
                 path.push("sessions");
                 path.set_extension("xml");
-                let parsed = Parser::from_file(path.as_path());
-                for session in parsed {
-                    if !has_session(&session.name) 
-                        && &session.name == &opt.session_name {
-                        session.unsafe_run(&mut Target::new()).unwrap();
-                    }
-                }
                 if opt.debug {
                     println!("[DEBUG] using config {}", path.as_path().display());
                 }
-            },
+            }
             None => panic!("Could not get home directory"),
         }
     } else {
         let path_buf = opt.config.unwrap();
         let path = path_buf.as_path();
         if path.exists() {
-            let parsed = Parser::from_file(path);
-            for session in parsed {
-                if !has_session(&session.name) && &session.name == &opt.session_name {
-                    session.unsafe_run(&mut Target::new()).unwrap();
-                }
-            }
+            let parsed = core::parser::Parser::from_file(path);
+            // todo: run set up
             if opt.debug {
                 println!("[DEBUG] using config {}", &path.display());
             }
--- core/src/tmux.rs
+++ core/src/tmux.rs
@@ -1,43 +1,57 @@
 use std::process::{Command, Output};
 use std::str;
 
+// todo: tmux commands don't need to be attached too, we can set up the env
 pub fn kill_session(target: &str) -> Result<String, String> {
-    let output = Command::new("tmux").arg("kill-session")
-        .arg("-t").arg(target)
+    let output = Command::new("tmux")
+        .arg("kill-session")
+        .arg("-t")
+        .arg(target)
         .output()
         .expect("failed to execute process");
     handle_output(output)
 }
 
 pub fn create_session(name: &str, window_name: &str) -> Result<String, String> {
-    let output = Command::new("tmux").arg("new-session").arg("-d")
-        .arg("-s").arg(name)
-        .arg("-n").arg(window_name)
+    let output = Command::new("tmux")
+        .arg("new-session")
+        .arg("-d")
+        .arg("-s")
+        .arg(name)
+        .arg("-n")
+        .arg(window_name)
         .output()
         .expect("failed to execute process");
     handle_output(output)
 }
 
 pub fn list_sessions(format: &str) -> Result<String, String> {
-    let output = Command::new("tmux").arg("list-session")
-        .arg("-F").arg(format)
+    let output = Command::new("tmux")
+        .arg("list-session")
+        .arg("-F")
+        .arg(format)
         .output()
         .expect("failed to execute process");
     handle_output(output)
 }
 
 pub fn new_window(target: &str, name: &str) -> Result<String, String> {
-    let output = Command::new("tmux").arg("new-window")
-        .arg("-t").arg(target)
-        .arg("-n").arg(name)
+    let output = Command::new("tmux")
+        .arg("new-window")
+        .arg("-t")
+        .arg(target)
+        .arg("-n")
+        .arg(name)
         .output()
         .expect("failed to execute process");
     handle_output(output)
 }
 
 pub fn select_layout(target: &str, layout: &str) -> Result<String, String> {
-    let output = Command::new("tmux").arg("select-layout")
-        .arg("-t").arg(target)
+    let output = Command::new("tmux")
+        .arg("select-layout")
+        .arg("-t")
+        .arg(target)
         .arg(layout)
         .output()
         .expect("failed to execute process");
@@ -45,11 +59,13 @@ pub fn select_layout(target: &str, layout: &str) -> Result<String, String> {
 }
 
 pub fn split_window(target: &str) -> Result<String, String> {
-    let output = Command::new("tmux").arg("split-window")
-        .arg("-t").arg(target)
+    let output = Command::new("tmux")
+        .arg("split-window")
+        .arg("-t")
+        .arg(target)
         .output()
         .expect("failed to execute process");
-     
+
     handle_output(output)
 }
 
@@ -63,11 +79,12 @@ fn handle_output(output: Output) -> Result<String, String> {
     }
 }
 
-
 pub fn send_command(target: &str, command: &str) -> Result<String, String> {
-    let output = Command::new("tmux").arg("send-keys")
-        .arg("-t").arg(target)
-		.arg(command)
+    let output = Command::new("tmux")
+        .arg("send-keys")
+        .arg("-t")
+        .arg(target)
+        .arg(command)
         .output()
         .expect("failed to execute process");
     handle_output(output)
@@ -87,20 +104,25 @@ pub fn has_session(name: &str) -> bool {
         .arg("-t")
         .arg(name)
         .output()
-        .expect("failed to execute process").status.success()
+        .expect("failed to execute process")
+        .status
+        .success()
 }
-
 
 #[derive(Debug, Clone)]
 pub struct Target {
-	session: Option<String>,
-	window: Option<String>,
-	pane: Option<String>
+    session: Option<String>,
+    window: Option<String>,
+    pane: Option<String>,
 }
 
 impl Target {
     pub fn new() -> Target {
-        Target { session: None, window: None, pane: None }
+        Target {
+            session: None,
+            window: None,
+            pane: None,
+        }
     }
 
     pub fn push(&mut self, name: &str) -> &mut Self {
@@ -116,30 +138,24 @@ impl Target {
         self
     }
 
-	pub fn from(session: &str, window: &str, pane: &str) -> Target {
-		Target { 
-			session: Some(String::from(session)), 
-			window: Some(String::from(window)),
-			pane: Some(String::from(pane))
-		}
-	}
+    pub fn from(session: &str, window: &str, pane: &str) -> Target {
+        Target {
+            session: Some(String::from(session)),
+            window: Some(String::from(window)),
+            pane: Some(String::from(pane)),
+        }
+    }
 
-	pub fn to_string(&self) -> String {
+    pub fn to_string(&self) -> String {
         match (&self.session, &self.window, &self.pane) {
             (None, None, None) => String::new(),
             (Some(session), None, None) => session.to_string(),
-            (Some(session), Some(window), None) => 
-                format!("{}:{}", session, window),
-            (Some(session), Some(window), Some(pane)) => 
-                format!("{}:{}.{}", session, window, pane),
-            (None, Some(window), None) => 
-                format!(":{}", window),
-            (None, Some(window), Some(pane)) => 
-                format!(":{}.{}", window, pane),
-            (None, None, Some(pane)) => 
-                format!(".{}", pane),
-            (Some(session), None, Some(pane)) => 
-                format!("{}.{}", session, pane),
+            (Some(session), Some(window), None) => format!("{}:{}", session, window),
+            (Some(session), Some(window), Some(pane)) => format!("{}:{}.{}", session, window, pane),
+            (None, Some(window), None) => format!(":{}", window),
+            (None, Some(window), Some(pane)) => format!(":{}.{}", window, pane),
+            (None, None, Some(pane)) => format!(".{}", pane),
+            (Some(session), None, Some(pane)) => format!("{}.{}", session, pane),
         }
-	}
+    }
 }
